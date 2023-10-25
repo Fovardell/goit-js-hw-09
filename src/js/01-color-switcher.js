b17da16125e1a643b13bef4825465f8a1a059ab3
Oleksandr Volokhov <Flatroniym@gmail.com>
2023-10-25 04:09:28 +0200

    добавив умови

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,9 @@
+/** Перед здачею домашнього завдання переконайтеся, що всі скрипти підключені та не закоментовані. Перевіряйте кожне виконане завдання відповідно з технічним завданням
+На 9 модулі ми продовжимо працювати з бібліотеками
+Об‘єкт Promise використовується для відкладених та асинхронних обчислень, і так як мова програмування javascript однопоточна, тому одночасно кілька дій він виконувати не може - для цього і існують асинхронні операції.
+Вам потрібно розібратися з тим, як працюють асинхронні функції і в який момент вони починають своє виконання. Які стани бувають у промісу і що вони означають, а також для чого потрібен метод then() при роботі з промісами.
+Для цього закріплюю для вас посилання з корисною інформацією.
+Асинхронний js -https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Introducing
+Об’єкт Promise - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+Метод then() -https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
+setTimeout, setInterval https://developer.mozilla.org/ru/docs/Web/API/WindowTimers/setTimeout https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval */
