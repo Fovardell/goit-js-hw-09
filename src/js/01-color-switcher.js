2da08f72dc12f831528f44dae6338f3afcacf658
Oleksandr Volokhov <Flatroniym@gmail.com>
2023-12-11 10:30:03 +0100

    виконав перше завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,22 @@
-/** Перед здачею домашнього завдання переконайтеся, що всі скрипти підключені та не закоментовані. Перевіряйте кожне виконане завдання відповідно з технічним завданням
-На 9 модулі ми продовжимо працювати з бібліотеками
-Об‘єкт Promise використовується для відкладених та асинхронних обчислень, і так як мова програмування javascript однопоточна, тому одночасно кілька дій він виконувати не може - для цього і існують асинхронні операції.
-Вам потрібно розібратися з тим, як працюють асинхронні функції і в який момент вони починають своє виконання. Які стани бувають у промісу і що вони означають, а також для чого потрібен метод then() при роботі з промісами.
-Для цього закріплюю для вас посилання з корисною інформацією.
-Асинхронний js -https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Introducing
-Об’єкт Promise - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-Метод then() -https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
-setTimeout, setInterval https://developer.mozilla.org/ru/docs/Web/API/WindowTimers/setTimeout https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval */
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const start = document.querySelector("button[data-start]");
+const stop = document.querySelector("button[data-stop]");
+let id;
+start.addEventListener("click", onStartButton);
+function onStartButton() {
+	id = setInterval(() => {
+		document.querySelector("body").style.backgroundColor = getRandomHexColor();
+	}, 1000);
+	start.setAttribute("disabled", true);
+	stop.removeAttribute('disabled');
+
+}
+
+stop.addEventListener('click', () => {
+	clearInterval(id);
+	start.removeAttribute('disabled');
+	stop.setAttribute("disabled", true);
+});
